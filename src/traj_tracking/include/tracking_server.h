a069cacdec50c9b31fe74aaafc09f76636cdcb67
wangdong <nobody@some.com>
2024-12-02 19:30:07 +0800

    [feat] add post-check method to check the feasibility of solution

--- src/traj_tracking/include/tracking_server.h
+++ src/traj_tracking/include/tracking_server.h
@@ -25,6 +25,7 @@ class TrackingServer {
         tracker_(TrajectoryTracker(mpc_param)),
         curve_generator_(curve_generator) {
     max_length_record_ = mpc_param.horizon_ * 2;
+    local_traj_pub_interval_ = mpc_param_.interval_;
   };
 
   void init(ros::NodeHandle &nh) {
@@ -50,7 +51,7 @@ class TrackingServer {
     //     nh.createTimer(ros::Duration(global_traj_pub_interval),
     //                    &TrackingServer::publishGlobalTrajectory, this);
     timer_local_traj_pub_ =
-        nh.createTimer(ros::Duration(local_traj_pub_interval),
+        nh.createTimer(ros::Duration(local_traj_pub_interval_),
                        &TrackingServer::publishLocalTrajectory, this);
     timer_contol_cmd_pub_ =
         nh.createTimer(ros::Duration(mpc_param_.interval_),
@@ -62,8 +63,8 @@ class TrackingServer {
     twist_cmd_pub_ = nh.advertise<geometry_msgs::Twist>(
         "/steer_bot/ackermann_steering_controller/cmd_vel", 10);
 
-    nh.param("/global_circle_radius", global_circle_radius_, 0.0);
-
+    nh.param("/traj_tracking/global_circle_radius", global_circle_radius_, 1.0);
+    ROS_INFO("global_circle_radius: %lf", global_circle_radius_);
     ros::Duration(5.0).sleep();
     publishGlobalCircleTrajectory();  // only publish once
     return;
@@ -72,8 +73,8 @@ class TrackingServer {
  private:
   const std::string file_path_ = "/tmp/ros/proto/traj_tracking/";
 
-  const double global_traj_pub_interval = 20.0;
-  const double local_traj_pub_interval = 0.10;
+  double global_traj_pub_interval_ = 20.0;
+  double local_traj_pub_interval_ = 0.10;
   double global_circle_radius_;
   ros::Subscriber odom_sub_;
   ros::Subscriber nav_point_sub_;
@@ -184,7 +185,8 @@ class TrackingServer {
     target_point_ = Eigen::Vector2d(0, global_circle_radius_);
     global_path_points_ = curve_generator_.getGlobalPath(global_circle_radius_);
     if (global_path_points_.empty()) {
-      ROS_ERROR("Failed to generate global path");
+      ROS_ERROR("Failed to generate global path, global circle radius = %lf",
+                global_circle_radius_);
       return;
     }
     visualization_msgs::Marker global_traj;
@@ -257,16 +259,13 @@ class TrackingServer {
     cmd.angular.x = 0;
     cmd.angular.y = 0;
     cmd.angular.z = 0;
+    TrajectoryTracker::DVector solution;
+    tracker_.update(current_state_, local_path_points_);
+    SolveStatus solve_status = tracker_.solve(solution);
     if (dist_to_target < threshold_near_target ||
         dist_off_path > threshold_far_off_path || local_path_points_.empty()) {
       ROS_INFO("Control command is not sent ...");
-      twist_cmd_pub_.publish(cmd);
-      return;
-    }
-    TrajectoryTracker::DVector solution;
-    tracker_.update(current_state_, local_path_points_);
-    bool solve_succeed = tracker_.solve(solution);
-    if (solve_succeed) {
+    } else if (solve_status == SolveStatus::SUCCESS) {
       cmd.linear.x =
           solution(mpc_param_.state_size_ * (mpc_param_.horizon_ + 1));
       cmd.linear.y = 0;
@@ -276,48 +275,59 @@ class TrackingServer {
       cmd.angular.z =
           solution(mpc_param_.state_size_ * (mpc_param_.horizon_ + 1) + 1);
     } else {
-      ROS_ERROR("Failed to solve the optimization problem");
+      if (solve_status == SolveStatus::SOLVER_INIT_ERROR) {
+        ROS_ERROR("Solver initialization error");
+      } else if (solve_status == SolveStatus::SOLVER_INNER_ERROR) {
+        ROS_ERROR("Solver inner error");
+      } else if (solve_status == SolveStatus::INVALID_SPEED) {
+        ROS_ERROR("Invalid speed");
+      } else if (solve_status == SolveStatus::INVALID_ACC) {
+        ROS_ERROR("Invalid acceleration");
+      } else if (solve_status == SolveStatus::INVALID_STEER_ANGLE) {
+        ROS_ERROR("Invalid steer angle");
+      } else if (solve_status == SolveStatus::INVALID_STEER_RATE) {
+        ROS_ERROR("Invalid steer rate");
+      }
     }
-    twist_cmd_pub_.publish(cmd);
 
     // save tracking data
-    if (solve_succeed) {
-      ros::Time cur_time = ros::Time::now();
-      std::stringstream ss;
-      ss << cur_time.sec << "_" << cur_time.nsec / 1e6;
-      tracking_data_.set_length(tracking_data_.length() + 1);
-      tracking_data_.add_timestamp(ss.str());
-      auto refer_data_ptr = tracking_data_.add_reference_data();
-      auto actual_data_ptr = tracking_data_.add_actual_data();
-      auto control_signal_ptr = tracking_data_.add_control_signal();
-      // referene state
-      Eigen::Vector3d refer_state;
-      Eigen::Vector2d refer_input;
-      tracker_.getCurrentReferStateAndInput(refer_state, refer_input);
-      // refer traj data
-      refer_data_ptr->set_x(refer_state(0));
-      refer_data_ptr->set_y(refer_state(1));
-      refer_data_ptr->set_theta(refer_state(2));
-      refer_data_ptr->set_v(refer_input(0));
-      refer_data_ptr->set_omega(refer_input(1));
-      refer_data_ptr->set_kappa(refer_input(1) / refer_input(0));
-      // actually data
-      actual_data_ptr->set_x(current_state_(0));
-      actual_data_ptr->set_y(current_state_(1));
-      actual_data_ptr->set_theta(current_state_(2));
-      actual_data_ptr->set_v(current_twist_(0));
-      actual_data_ptr->set_omega(current_twist_(1));
-      actual_data_ptr->set_kappa(current_twist_(1) / current_twist_(0));
-      // control data
-      double v = cmd.linear.x, omega = cmd.angular.z;
-      control_signal_ptr->set_v(v);
-      control_signal_ptr->set_omega(omega);
-      control_signal_ptr->set_kappa(omega / v);
-    }
+    ros::Time cur_time = ros::Time::now();
+    std::stringstream ss;
+    ss << cur_time.sec << "_" << cur_time.nsec / 1e6;
+    tracking_data_.set_length(tracking_data_.length() + 1);
+    tracking_data_.add_timestamp(ss.str());
+    auto refer_data_ptr = tracking_data_.add_reference_data();
+    auto actual_data_ptr = tracking_data_.add_actual_data();
+    auto control_signal_ptr = tracking_data_.add_control_signal();
+    // referene state
+    Eigen::Vector3d refer_state;
+    Eigen::Vector2d refer_input;
+    tracker_.getCurrentReferStateAndInput(refer_state, refer_input);
+    // refer traj data
+    refer_data_ptr->set_x(refer_state(0));
+    refer_data_ptr->set_y(refer_state(1));
+    refer_data_ptr->set_theta(refer_state(2));
+    refer_data_ptr->set_v(refer_input(0));
+    refer_data_ptr->set_omega(refer_input(1));
+    refer_data_ptr->set_kappa(refer_input(1) / refer_input(0));
+    // actually data
+    actual_data_ptr->set_x(current_state_(0));
+    actual_data_ptr->set_y(current_state_(1));
+    actual_data_ptr->set_theta(current_state_(2));
+    actual_data_ptr->set_v(current_twist_(0));
+    actual_data_ptr->set_omega(current_twist_(1));
+    actual_data_ptr->set_kappa(current_twist_(1) / current_twist_(0));
+    // control data
+    double v = cmd.linear.x, omega = cmd.angular.z;
+    control_signal_ptr->set_v(v);
+    control_signal_ptr->set_omega(omega);
+    control_signal_ptr->set_kappa(omega / v);
+
     if (tracking_data_.length() == max_length_record_) {
       serialize();
       tracking_data_.Clear();
     }
+    twist_cmd_pub_.publish(cmd);
   }
   void serialize() {
     std::string time_stamp;
